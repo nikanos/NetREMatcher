2142313c9ff85f256932f72357112bdedc7a9385
nikanos <nikanos@windowslive.com>
2017-06-11 23:25:37 +0300

    Add project files.

---
+++ NetREMatcher/Options.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace NetREMatcher
+{
+    class Options
+    {
+        [Option('i', "input", Required = false, HelpText = "Input file name")]
+        public string InputFile { get; set; }
+
+        [Option('o', "output", Required = false, HelpText = "Output file Name")]
+        public string OutputFile { get; set; }
+
+        [Option('p', "pattern", Required = false, HelpText = "Pattern", MutuallyExclusiveSet = "pattern")]
+        public string Pattern { get; set; }
+
+        [Option('f', "pattern-file", Required = false, HelpText = "Pattern file name", MutuallyExclusiveSet = "pattern")]
+        public string PatternFile { get; set; }
+
+        [Option('v', "invert-results", Required = false, HelpText = "Invert Results")]
+        public bool InvertResults { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText
+            {
+                Heading = new HeadingInfo("NetREMatcher"),
+                AdditionalNewLineAfterOption = false,
+                AddDashesToOption = true
+            };
+            help.AddPostOptionsLine("Pattern and Pattern file name are mutually exclusive.");
+            help.AddPostOptionsLine("Either Pattern or Pattern file name must be set.");
+            help.AddOptions(this);
+            return help;
+
+        }
+
+    }
+}
---
+++ NetREMatcher/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace NetREMatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new CommandLine.Parser(opt =>
+            {
+                opt.MutuallyExclusive = true;
+                opt.HelpWriter = Console.Error;
+            });
+            var options = new Options();
+            if (parser.ParseArguments(args, options))
+            {
+                if (string.IsNullOrEmpty(options.Pattern) && string.IsNullOrEmpty(options.PatternFile))
+                {
+                    Console.Error.WriteLine(options.GetUsage());
+                    Environment.Exit(-1);
+                }
+                string pattern = !string.IsNullOrEmpty(options.Pattern) ? options.Pattern : File.ReadAllText(options.PatternFile);
+                Regex re = new Regex(pattern);
+                using (TextReader tr = (!string.IsNullOrEmpty(options.InputFile) ? new StreamReader(options.InputFile) : Console.In))
+                {
+                    using (TextWriter tw = (!string.IsNullOrEmpty(options.OutputFile) ? new StreamWriter(options.OutputFile) : Console.Out))
+                    {
+                        string line;
+                        while (null != (line = tr.ReadLine()))
+                        {
+                            bool matchResult = re.IsMatch(line);
+                            matchResult = options.InvertResults ? !matchResult : matchResult;
+                            if (matchResult)
+                                tw.WriteLine(line);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
